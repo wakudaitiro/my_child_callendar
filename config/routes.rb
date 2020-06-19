bb95be0d446ff1d59b4e20a9871b4f1c24476e26
wakudaitiro <wakudaitiro@gmail.com>
2020-06-19 12:05:44 +0900

    routing追加 root visitorのindex_view Eventの各種routig追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'visitors/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'visitors/index'
+  resources :events
 end
