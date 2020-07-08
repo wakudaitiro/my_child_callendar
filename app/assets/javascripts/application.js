d1c2e1882a17f2068a46fc376106058e66d7229a
wakudaitiro <wakudaitiro@gmail.com>
2020-07-08 23:38:34 +0900

    ドラックした時間幅の時間モーダル表示 デリートした後にすぐに反映 イベントの終了時刻をイベントエレメントに表示（今はコメントアウト）

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,17 +20,13 @@
 //= require moment/ja.js
 //= require_tree .
 
-
-
-
-
-$(document).ready(function() {
+$(document).ready(function () {
   "use strict";
   // ------------------------------------------------------- //
   // Calendar
   // ------------------------------------------------------ //
-  var initialize_calendar = function() {
-    $('#calendar').each(function() {
+  var initialize_calendar = function () {
+    $("#calendar").each(function () {
       var calendar = $(this);
       calendar.fullCalendar({
         themeSystem: "bootstrap4",
@@ -43,78 +39,237 @@ $(document).ready(function() {
         selectHelper: true,
         selectable: true,
         ignoreTimezone: false,
+        snapDuration: "00:15:00",
+        
+
         // header
         header: {
           left: "title",
           center: "month,agendaWeek,agendaDay",
           right: "today prev,next",
         },
-        events: '/events.json',
+        events: "/events.json",
 
         select: function (start, end) {
-          $.getScript("/events/new", function (start, end) {
-            $("#datetimepicker7").datetimepicker();
+          $.getScript("/events/new", function () {
+            $("datetimepicker7").datetimepicker({
+              useCurrent: false,
+            });
             $("#datetimepicker8").datetimepicker({
-              useCurrent: false
+              useCurrent: false,
             });
             $("#datetimepicker7").on("change.datetimepicker", function (start) {
-                $('#datetimepicker8').datetimepicker('minDate', start.date);
+              $("#datetimepicker8").datetimepicker("minDate", start.date);
             });
             $("#datetimepicker8").on("change.datetimepicker", function (end) {
-                $('#datetimepicker7').datetimepicker('maxDate', end.date);
+              $("#datetimepicker7").datetimepicker("maxDate", end.date);
             });
-
-            $('#add-event').submit(function() {
-              alert("Submitted");
-              var values = {};
-              $.each($('#add-event').serializeArray(), function(i, field) {
-                  values[field.name] = field.value;
-              });
-              console.log(
-                values
-              );
-            })
+            $(".input-start").val(moment(start).format("YYYY-MM-DD HH:mm"));
+            $(".input-end").val(moment(end).format("YYYY-MM-DD HH:mm"));
           }),
-  
-          calendar.fullCalendar("unselect");
+            calendar.fullCalendar("unselect");
         },
 
-        eventDrop: function(event, delta, revertFunc) {
+        eventDrop: function (event) {
           var event_data = {
             event: {
               id: event.id,
               start: event.start.format(),
-              end: event.end.format()
-            }
+              end: event.end.format(),
+            },
           };
 
-          var update_url = "/events/"+event.id
+          var update_url = "/events/" + event.id;
           $.ajax({
-              url: update_url,
-              data: event_data,
-              type: 'PATCH',
+            url: update_url,
+            data: event_data,
+            type: "PATCH",
           });
         },
 
-        eventClick: function(event, jsEvent, view) {
-          $.getScript(event.edit_url, function() {
-            $('.start_hidden').val(moment(event.start).format('YYYY-MM-DD HH:mm'));
-            $('.end_hidden').val(moment(event.end).format('YYYY-MM-DD HH:mm'));
-            $(".event-icon").html("<i class='fa fa-" + event.icon + "'></i>");
-            $(".event-title").html(event.title);
-            $("#modal-view-event").modal();
+        eventClick: function (event, jsEvent, view) {
+          // $.getScript(event.edit_url, function() {
+          //   $('.start_hidden').val(moment(event.start).format('YYYY-MM-DD HH:mm'));
+          //   $('.end_hidden').val(moment(event.end).format('YYYY-MM-DD HH:mm'));
+          //   $(".event-icon").html("<i class='fa fa-" + event.icon + "'></i>");
+          //   $(".event-title").html(event.title);
+          //   $("#modal-view-event").modal();
+          // });
+
+          var edit_url = "/events/" + event.id + "/edit";
+          $.getScript(edit_url, function () {
+            $("#datetimepicker7").datetimepicker();
+            $("#datetimepicker8").datetimepicker({
+              useCurrent: false,
+            });
+            $("#datetimepicker7").on("change.datetimepicker", function (start) {
+              $("#datetimepicker8").datetimepicker("minDate", start.date);
+            });
+            $("#datetimepicker8").on("change.datetimepicker", function (end) {
+              $("#datetimepicker7").datetimepicker("maxDate", end.date);
+            });
           });
-        }
+        },
+
+        
+        eventRender: function (event, element) {
+          if (event.icon) {
+            element
+            .find(".fc-title")
+            .prepend(
+              '<img src=' + event.icon + ' class="event-img"/>'
+              );
+            }
+          },
+          
+          displayEventTime: true,
+          // displayEventEnd: {
+          //   month: true,
+          //   basicWeek:true,
+          //   "Default": true
+          // },
+          timeFormat: "HH:mm",
+          
+          
+        });
       });
-    })
-  };
-  $(document).on('turbolinks:load', initialize_calendar);
-})
-
-
-
-//   $(function () {
-//     // page is ready
+    };
+    $(document).on("turbolinks:load", initialize_calendar);
+  });
+  
+  
+  
+  //         events: [
+  //   {
+  //     title: "Barber",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2020-07-07T14:00:00",
+  //     end: "2020-07-07T15:00:00",
+  //     className: "fc-bg-default",
+  //     icon: "https://img.icons8.com/plasticine/100/000000/milk-bottle.png",
+  //   },
+  //   {
+  //     title: "Flight Paris",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2020-07-08T14:00:00",
+  //     end: "2020-07-08T20:00:00",
+  //     className: "fc-bg-deepskyblue",
+  //     icon: "cog",
+  //     allDay: false,
+  //   },
+  //   {
+  //     title: "Team Meeting",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2020-07-10T13:00:00",
+  //     end: "2020-07-10T16:00:00",
+  //     className: "fc-bg-pinkred",
+  //     icon: "group",
+  //     allDay: false,
+  //   },
+  //   {
+  //     title: "Meeting",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-08-12",
+  //     className: "fc-bg-lightgreen",
+  //     icon: "suitcase",
+  //   },
+  //   {
+  //     title: "Conference",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-08-13",
+  //     end: "2019-08-15",
+  //     className: "fc-bg-blue",
+  //     icon: "calendar",
+  //   },
+  //   {
+  //     title: "Baby Shower",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2020-07-13",
+  //     end: "2020-07-14",
+  //     className: "fc-bg-default",
+  //     icon: "child",
+  //   },
+  //   {
+  //     title: "Birthday",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-09-13",
+  //     end: "2019-09-14",
+  //     className: "fc-bg-default",
+  //     icon: "birthday-cake",
+  //   },
+  //   {
+  //     title: "Restaurant",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-10-15T09:30:00",
+  //     end: "2019-10-15T11:45:00",
+  //     className: "fc-bg-default",
+  //     icon: "glass",
+  //     allDay: false,
+  //   },
+  //   {
+  //     title: "Dinner",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-11-15T20:00:00",
+  //     end: "2019-11-15T22:30:00",
+  //     className: "fc-bg-default",
+  //     icon: "cutlery",
+  //     allDay: false,
+  //   },
+  //   {
+  //     title: "Shooting",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-08-25",
+  //     end: "2019-08-25",
+  //     className: "fc-bg-blue",
+  //     icon: "camera",
+  //   },
+  //   {
+  //     title: "Go Space :)",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-12-27",
+  //     end: "2019-12-27",
+  //     className: "fc-bg-default",
+  //     icon: "rocket",
+  //   },
+  //   {
+  //     title: "Dentist",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2019-12-29T11:30:00",
+  //     end: "2019-12-29T012:30:00",
+  //     className: "fc-bg-blue",
+  //     icon: "medkit",
+  //     allDay: false,
+  //   },
+  //   {
+  //     title: "Dentist",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
+  //     start: "2020-06-27T11:30:00",
+  //     end: "2020-06-28T012:30:00",
+  //     className: "fc-bg-blue",
+  //     icon: "medkit",
+  //     allDay: false,
+  //   },
+  // ],
+//   eventAfterRender: function(event, element, view) {
+//     $(element).css('width','50%');
+//  },
+  
+  
+  //   $(function () {
+    //     // page is ready
 //     $("#calendar").fullCalendar({
 //       themeSystem: "bootstrap4",
 //       // emphasizes business hours
@@ -137,8 +292,8 @@ $(document).ready(function() {
 //           title: "Barber",
 //           description:
 //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.",
-//           start: "2019-07-07",
-//           end: "2019-07-07",
+//           start: "2019-07-07T14:00:00",
+//           end: "2019-07-07T15:00:00",
 //           className: "fc-bg-default",
 //           icon: "circle",
 //         },
