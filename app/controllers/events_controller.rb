41ee856dff4ce97813f405cd58f3aac27e2d71fe
wakudaitiro <wakudaitiro@gmail.com>
2020-07-14 21:13:21 +0900

    グラフが一週間分表示されるようにした。 config.active_record.default_timezoneでDB保存時の時間を変更 違うアイコンでもグラフが表示されるようにしたい（まだ未実装）

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: %i[show edit update destroy]
 
   def index
     @events = Event.where(start: params[:start]..params[:end])
@@ -7,15 +7,13 @@ class EventsController < ApplicationController
     render json: json.to_json
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @event = Event.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @event = Event.new(event_params)
@@ -31,15 +29,80 @@ class EventsController < ApplicationController
   end
 
   def statics
+    # @date = l Event.start
+    start_date = 1.week.ago.beginning_of_day
+    end_date = Time.zone.now.end_of_day
+    week_ragne = start_date..end_date
+    order_icons = 
+    # @milk = Event.order('start').where(icon: 'milk.png').pluck(:start).group_by { |start| I18n.l(start) }
+    # .map do |date,array|
+    #   [date, array.count]
+    # end
+
+    # query = "SELECT to_char(start,'MM-DD') AS start_date, count(*)
+    #          FROM
+    #            events
+    #          WHERE icon='milk.png'
+    #          GROUP BY to_char(start,'MM-DD'), icon"
+
+    # sql = <<-"EOS"
+    # SELECT
+
+    # EOS
+    
+    # sqlを実行し、取得結果をhashに変換
+    # ActiveRecord::Base.connection.select_all(sql)
+    # @count = Event.find_by_sql(query)
+
+    # @order_icons.pluck(:start).group_by{ |start| I18n.l(start) }.map do |hour, array|
+
+    # @icons = order_icons.group_by(start)
+    # @icons = Event.group_by{ |start| I18n.l(start) }.order("start").pluck(:start, :icon)
+
+
+
+#     sql = <<-"EOS"
+# SELECT d.start_date, coalesce(r.count, 0) AS count
+# FROM (
+#   SELECT to_char(s.a, 'MM-DD') AS start_date FROM (
+#     SELECT * FROM generate_series(current_timestamp + '-6 days', current_timestamp, '1 day'::interval)
+#   ) AS s(a)
+# ) AS d
+# LEFT JOIN (
+#   SELECT to_char(start,'MM-DD') AS start_date, count(*)
+#   FROM
+#     events
+#   WHERE icon='milk.png'
+#   GROUP BY to_char(start,'MM-DD'), icon
+# ) AS r ON d.start_date = r.start_date
+# EOS
+
+#     result = ActiveRecord::Base.connection.select_all(sql)
+    result = Event.milk_count
+    @milk = result::rows
+    # binding.pry 
+
+    
+
+
+
+
+    # @icons = Event.order("start").pluck(:start, :icon)
     # @day_count = @events.group('HOUR(created_at)').count
+    # @day_count = @events.joins(:start).group(:start)
+    # @week_count =
+
+
+
   end
 
   private
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    def event_params
-      params.require(:event).permit(:title, :start, :end, :color, :icon)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :start, :end, :color, :icon)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module MyChildCallendar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
