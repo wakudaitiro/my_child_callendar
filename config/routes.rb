ff03a63f27dcc25c3e99fb00bf47860d6ba754c9
wakudaitiro <wakudaitiro@gmail.com>
2020-07-15 21:07:16 +0900

    routing修正、検索して商品が出るようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'products/index'
-  get 'products/search'
   # root to: 'visitors#index'
   get 'index', to: 'visitors#index'
   get 'statics', to: 'events#statics'
-  get 'products', to: 'products#index'
-  get 'products', to: 'products#search'
+  # get 'products', to: 'products#index'
+  get 'search', to: 'products#search'
   resources :events
+  resources :products
 end
