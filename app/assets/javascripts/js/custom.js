1bc759ffd2189da67fac45596d8e3a36e0ab179c
wakudaitiro <wakudaitiro@gmail.com>
2020-08-06 14:13:25 +0900

    デフォルト表示を週毎のカレンダーに変更、applicationn.htmlの空白行などを削除

--- app/assets/javascripts/js/custom.js
+++ app/assets/javascripts/js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         displayEventTime: true,
         timeFormat: "HH:mm",
         businessHours: false,
-        defaultView: "agendaDay",
+        defaultView: "agendaWeek",
         editable: true,
         selectHelper: true,
         selectable: true,
