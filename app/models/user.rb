a71ab402be7fb2e4d233abc7f9c2157e4a14c710
wakudaitiro <wakudaitiro@gmail.com>
2020-08-08 23:12:30 +0900

    emailでuniquenessのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true
   validates :username, presence: true, length: { maximum: 30 }
   validates :babyname, presence: true, length: { maximum: 30 }
 
