376f8ba254cbd88c4cc05e5d4798e228845c27d3
wakudaitiro <wakudaitiro@gmail.com>
2020-08-11 09:59:17 +0900

    ユーザーアカウント削除、関連イベント削除のテスト追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'UserAuthentications', type: :request do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
+  let!(:evnet) { create(:event, user_id: user.id) }
   let!(:login_user) { create(:login_user) }
   let(:user_params) { attributes_for(:user) }
   let(:login_user_params) { attributes_for(:login_user) }
@@ -19,7 +20,7 @@ RSpec.describe 'UserAuthentications', type: :request do
       end
     end
 
-    context 'パラメータが不正な場合' do
+    context 'メールアドレスが異なる場合' do
       it 'ページ遷移しないこと' do
         post user_session_path, params: { user: nologin_user_params }
         expect(response.status).to eq 200
@@ -37,13 +38,38 @@ RSpec.describe 'UserAuthentications', type: :request do
       end
     end
 
-    context 'パラメータが不正な場合' do
+    context 'メールアドレスの入力がない場合' do
       it 'ユーザー登録されない、ページ遷移しないこと' do
-        post user_registration_path, params: { user: login_user_params }
         expect do
-          post user_registration_path, params: { user: login_user_params }
+          post user_registration_path, params: { user: invalid_user_params }
         end.to change(User, :count).by 0
         expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'ユーザー削除' do
+    context 'ログインしている場合' do
+      it 'ユーザーが削除され,トップページにリダイレクトされること' do
+        sign_in login_user
+        expect do
+          delete user_registration_path, params: { user: login_user_params }
+        end.to change(User, :count).by(-1)
+        expect(response).to redirect_to root_url
+      end
+
+      it 'ユーザー削除と同時に関連したイベントが削除されること' do
+        sign_in user
+        expect { user.destroy }.to change { Event.count }.by(-1)
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it '削除ができず、ログイン画面にリダイレクトされること' do
+        expect do
+          delete user_registration_path, params: { user: login_user_params }
+        end.to change(User, :count).by 0
+        expect(response).to redirect_to new_user_session_url
       end
     end
   end
